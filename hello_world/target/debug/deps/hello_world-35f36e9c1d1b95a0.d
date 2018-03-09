a64316978384bbafa8ed1517040d232a10cc07f9
Robby <ta7382@gmail.com>
2018-03-09 15:45:48 +0800

    Update cargo.toml

---
+++ hello_world/target/debug/deps/hello_world-35f36e9c1d1b95a0.d
@@ -0,0 +1,5 @@
+D:\Project\Rust\RustTutorial\hello_world\target\debug\deps\hello_world-35f36e9c1d1b95a0.exe: src\main.rs
+
+D:\Project\Rust\RustTutorial\hello_world\target\debug\deps\hello_world-35f36e9c1d1b95a0.d: src\main.rs
+
+src\main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+D:\Project\Rust\RustTutorial\hello_world\target\debug\hello_world.exe: D:\Project\Rust\RustTutorial\hello_world\src\main.rs
